1956698a0d0c914b719d56f7ef0bd28ca08213b6
Boris Gusev <bgusev@gmail.com>
2021-05-19 18:48:45 +0200

    added comments to exploration.py

--- exploration.py
+++ exploration.py
@@ -5,18 +5,17 @@ from skimage import feature
 from skimage import color
 from skimage import filters
 
+# assumes that the unzipped training folder is included in the directory
 train_data = Path('Train-Data')
 healthy_images = [img_path for img_path in (train_data / 'NoSRF').iterdir()]
 srf_images = [img_path for img_path in (train_data / 'SRF').iterdir()]
 
-img = plt.imread(healthy_images[0])
+# placeholder to view individual images
+# img = plt.imread(healthy_images[0])
 img = plt.imread(srf_images[9])
 img = color.rgba2rgb(img)
 img = color.rgb2gray(img)
 edge = filters.sobel(filters.gaussian(img, sigma=5))
-
-# img = color.rgb2gray(img)
-# edge = filters.sobel_v(img)
 
 plt.subplot(1, 2, 1)
 plt.imshow(img, cmap='gray')
