850efeec4254cee86fa84a0a8927f0510a9b2f9b
disolis <30530148+disolis@users.noreply.github.com>
2021-05-25 19:03:36 +0200

    Add files via upload

--- image_edit_utils.py
+++ image_edit_utils.py
@@ -11,6 +11,36 @@ from scipy import ndimage
 from sklearn import cluster
 
 
+
+# def edge_detection (gray_scaled_image, edges, threshold=0.1):
+#     segmented_image = segmentation(gray_scaled_image, nclust=7)
+#     segmented_image = color.rgb2gray(segmented_image)
+
+#     h = gray_scaled_image.shape[0]
+#     hpx = int(h*threshold)
+#     print (hpx)
+#     error = 0.001
+#     target_segment = np.max(segmented_image) # target layer
+#     print (target_segment)
+#     for i in range (edges.shape[0]):
+#         for j in range (edges.shape[1]):
+#             if (edges[i,j]==True):
+#                 status = False
+#                 for k in range (i-hpx,i):
+#                    if (np.abs(segmented_image[k,j]-target_segment)<error):
+#                        status = True
+#                        break
+#                 for k in range (i,i+hpx):
+#                    if (np.abs(segmented_image[k,j]-target_segment)<error):
+#                        status = True
+#                        break
+#                 edges[i,j]=status
+
+#     return (edges)                
+
+
+
+# Remove white frame
 def rm_white_frame (im):
 
     # create a negative (to invert the white frame into black, to fill with 0)
@@ -41,7 +71,7 @@ def rm_white_frame (im):
 
     return (cropped)    
 
-def segmentation(gray_image): 
+def segmentation(gray_image, nclust=7): 
 
     image = color.gray2rgb(gray_image)
 
@@ -49,52 +79,25 @@ def segmentation(gray_image):
     image_2d = image.reshape(x*y, z)
     image_2d.shape
 
-    kmeans_cluster = cluster.KMeans(n_clusters=7)
+    kmeans_cluster = cluster.KMeans(n_clusters=nclust)
     kmeans_cluster.fit(image_2d)
     cluster_centers = kmeans_cluster.cluster_centers_
     cluster_labels = kmeans_cluster.labels_
     image_segmented = cluster_centers[cluster_labels].reshape(x, y, z)
 
-    return (image_segmented)
+    cluster_labels_matrix = cluster_labels.reshape(x, y)
 
-    plt.show()
-
-
-
-def rm_bg2(gray_img): 
+    return (image_segmented, cluster_labels_matrix)
 
 
-    _, thresh = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    img_contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-
-    img_contours = sorted(img_contours, key=cv2.contourArea)
-
-   
-    for i in img_contours:
-        if cv2.contourArea(i) > 10000000000:
-            break
-
-
-    mask = np.zeros(gray_img.shape[:2], np.uint8)
-    cv2.drawContours(mask, [i],-1, 255, -1)
-
-    # mask[np.where(mask==255)]=100
-    # mask[np.where(mask==0)]=255
-    # mask[np.where(mask==100)]=0
-
-    new_img = cv2.bitwise_and(gray_img, gray_img, mask=mask)
-
-    return (mask)
-
-
-def filter_edges(edges, gray_image):
-    print (np.max(gray_image))
-    for i in range(edges.shape[0]):
-        for j in range(edges.shape[1]):
-            if (i-1>0 and i<edges.shape[0]-1):
-                if (gray_image[i-1,j]<130 and gray_image[i+1,j]<130):
-                    edges[i,j]=False
+# def filter_edges(edges, gray_image):
+#     print (np.max(gray_image))
+#     for i in range(edges.shape[0]):
+#         for j in range(edges.shape[1]):
+#             if (i-1>0 and i<edges.shape[0]-1):
+#                 if (gray_image[i-1,j]<130 and gray_image[i+1,j]<130):
+#                     edges[i,j]=False
         
-    return (edges)
+#     return (edges)
 
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-import sys
 import matplotlib
-from matplotlib import pyplot as plt
-from pathlib import Path
+import matplotlib.pyplot as plt
+import get_file_paths
 
 from skimage import feature
 from skimage import color
 from skimage import filters
 from skimage import util
 
-import os
 from scipy.ndimage import gaussian_filter
-import image_edit_utils as utls
 from scipy import ndimage
+
+import image_edit_utils as utls
 
 
 # *OPTIONAL
@@ -45,88 +42,85 @@ def apply_brightness_contrast(input_img, brightness = 0, contrast = 0):
 
 # Read in the image
 
-# assumes that the unzipped training folder is included in the directory
-train_data = Path('Train-Data')
-# healthy_images = [img_path for img_path in ('NoSRF').iterdir()]
-srf_images = [img_path for img_path in (train_data / 'SRF').iterdir()]
 
-train_data = Path('Train-Data')
+healthy, srf = get_file_paths.get_all_train_data()
 
-# placeholder to view individual images
-# image_original = plt.imread(healthy_images[0])
-image_original = plt.imread(srf_images[9])
+for img_path in srf:
+    image_original = plt.imread(img_path)
 
 
-#image_original = plt.imread("SRF/input_1647_1.png")
-# Remove white frame around
-image_original = utls.rm_white_frame(image_original)
+    # Remove white frame around the image (interfere with edge detection, the edges are detected on the border)
+    image_original = utls.rm_white_frame(image_original)
 
-plt.subplot(221)
-plt.title("Original")
-plt.imshow(image_original)
-
-
-# improve quality of image
-#image_enhanced = apply_brightness_contrast(image_original, brightness = 10, contrast = 15)
-image_gray = color.rgba2rgb(image_original)
-image_gray = color.rgb2gray(image_gray)
-
-gaus_par=3
-image_gray_gaus = ndimage.gaussian_filter(image_gray, gaus_par)
-
-utls.segmentation(image_gray_gaus)
-
-plt.subplot(222)
-plt.title("Gaussian smoothing with par ="+str(gaus_par))
-plt.imshow(image_gray_gaus)
+    plt.subplot(221)
+    plt.title("Original")
+    plt.imshow(image_original)
 
 
+    # improve quality of image
+    #image_enhanced = apply_brightness_contrast(image_original, brightness = 10, contrast = 15)
+    image_gray = color.rgba2rgb(image_original)
+    image_gray = color.rgb2gray(image_gray)
 
-# # Sobel filter
+    gaus_par=3
+    image_gray_gaus = ndimage.gaussian_filter(image_gray, gaus_par)
 
-#image_gray_gaus = image_gray_gaus.astype('int32')
+    plt.subplot(222)
+    plt.title("Gaussian smoothing with par ="+str(gaus_par))
+    plt.imshow(image_gray_gaus)
 
-# sx = ndimage.sobel(image_gray_gaus, axis=0, mode="mirror")
-# sy = ndimage.sobel(image_gray_gaus, axis=1, mode="mirror")
-# image_gray_gaus_sobel = np.hypot(sx, sy)
-# image_gray_gaus_sobel *= 255.0 / np.max(image_gray_gaus_sobel) # normalization 0..255
-# image_gray_gaus_sobel = np.float32(image_gray_gaus_sobel)
-
-segmented_image = utls.segmentation(image_gray_gaus)
-
-plt.subplot(223)
-plt.title("Segmentation")
-plt.imshow(segmented_image)
-
-# image_gray_gaus = np.float32(image_gray_gaus)
-
-sigma_val=3
-edges = feature.canny(image=image_gray_gaus, sigma = sigma_val)  
-
-plt.subplot(224)
-plt.title("Edge detection with sigma = "+str(sigma_val))
-plt.imshow(edges,cmap='gray')
-
-plt.show()
+    # Segmentation
+    # cluster_labels is a matrix of image shape, 
+    # where each cell value corresponds to the segment number of pixel,
+    # i.e. 0 - a pixel of darkest segment; (nclust-1) - a pixel of the brightest segment
+    # nclust - number of clusters 
+    n = 7
+    segmented_image, cluster_labels = utls.segmentation(image_gray_gaus, nclust=n)
 
 
-y_edges=np.where(edges)[0]
-x_edges=np.where(edges)[1]
+    plt.subplot(223)
+    plt.title("Segmentation with nclust = "+ str(n))
+    plt.imshow(segmented_image)
 
 
-for i in range(len(y_edges)):
-    if (y_edges[i]!=0 or x_edges[i]!=0 or y_edges[i]!=segmented_image.shape[0] or x_edges[i]!=segmented_image.shape[1]):
-        segmented_image[y_edges[i],x_edges[i]]=(1,0,0)
+    sigma_val=3
+    # edge detection
+    edges = feature.canny(image=image_gray_gaus, sigma = sigma_val)  
 
-plt.subplot(121)
-plt.title("Original")
-plt.imshow(image_original)
+    # # get rif of the topmost edge 
+    # for j in range(edges.shape[1]):
+    #     for i in range(edges.shape[0]):
+    #         if (edges[i,j]==True):
+    #             edges[i,j]=False
+    #             break
 
-plt.subplot(122)
-plt.title("Segmented image with edges")
-plt.imshow(segmented_image)
 
-plt.show()
+    plt.subplot(224)
+    plt.title("Edge detection with sigma = "+str(sigma_val))
+    plt.imshow(edges,cmap='gray')
+
+    plt.show()
+
+    segmented_image_with_edges = np.copy(segmented_image)
+
+    y_edges=np.where(edges)[0]
+    x_edges=np.where(edges)[1]
+
+    for i in range(len(y_edges)):
+        if (y_edges[i]!=0 or x_edges[i]!=0 or y_edges[i]!=segmented_image_with_edges.shape[0] or x_edges[i]!=segmented_image_with_edges.shape[1]):
+            segmented_image_with_edges[y_edges[i],x_edges[i]]=(1,0,0) # possibly will help......
+
+
+    # Plot Segmented image with detected edges
+    plt.subplot(121)
+    plt.title("Original")
+    plt.imshow(image_original)
+
+    plt.subplot(122)
+    plt.title("Segmented image with edges")
+    plt.imshow(segmented_image_with_edges)
+
+    plt.show()
 
 
 
