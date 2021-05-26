4b2b3c64617d7f9629ec2e737b9e82dd4f62cc85
Boris Gusev <bgusev@gmail.com>
2021-05-26 20:00:40 +0200

    basic blob-counting approach for detection
    also added tqdm for feedback on imaga analysis progress

--- utils/detection.py
+++ utils/detection.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 import get_file_paths, image_preprocessing, find_blobs
 
@@ -7,14 +8,29 @@ if __name__ == '__main__':
     healthy, srf = get_file_paths.get_all_train_data()
 
     no_srf_detected = 0
-    for img_path in healthy:
+    for img_path in tqdm(healthy):
         img = plt.imread(img_path)
         img = image_preprocessing.preprocess(img)
         blobs = find_blobs.find_candidate_srf_blobs(img)
         blobs = find_blobs.filter_blob_candidates(img, blobs)
 
-        print(len(blobs))
         if len(blobs) == 0:
             no_srf_detected += 1
 
-    print(f'In {no_srf_detected} healthy images no SRF was detected.')
+    print(f'Negative result for SRF was correctly returned for {no_srf_detected} out of {len(healthy)} healthy images')
+
+
+    srf_detected = 0
+    for img_path in tqdm(srf):
+        img = plt.imread(img_path)
+        img = image_preprocessing.preprocess(img)
+        blobs = find_blobs.find_candidate_srf_blobs(img)
+        blobs = find_blobs.filter_blob_candidates(img, blobs)
+
+        if len(blobs) != 0:
+            srf_detected += 1
+
+    
+    print(f'Positive result for SRF was correctly returned for {srf_detected} out of {len(srf)} srf images')
+
+        
