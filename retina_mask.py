efc740ab5de8e4f69aca88299e8a9fe8d3a9e760
Boris Gusev <bgusev@gmail.com>
2021-05-26 16:35:34 +0200

    script to create mask for retina
    May delete later if remains a single function, incorporating the function into a larger script

---
+++ retina_mask.py
@@ -0,0 +1,35 @@
+from skimage import color, feature, filters, segmentation, morphology, measure
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage as ndi
+
+import image_preprocessing
+import get_file_paths
+import find_blobs
+import image_edit_utils as utls
+
+healthy, srf = get_file_paths.get_all_train_data()
+
+def retinal_mask(img):
+    mask = np.zeros_like(img)
+    blur = filters.gaussian(img, sigma = 10)
+    mask[blur > 0.25] = 1
+    mask = ndi.binary_fill_holes(mask)
+    return mask
+
+# img = plt.imread(srf[4])
+for img in srf:
+    img = plt.imread(img)
+    img = image_preprocessing.preprocess(img)
+
+    # img = morphology.opening(img)
+
+    # edges = get_retinal_mask(img)
+    edges = retinal_mask(img)
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(img, cmap='gray')
+    plt.subplot(1, 2, 2)
+    plt.imshow(edges, cmap='gray')
+
+    plt.show()
