51af8b911ddc21bb3f459bef7bfb7bde255c9297
Boris Gusev <bgusev@gmail.com>
2021-05-29 22:38:05 +0200

    changed to only glob .png files

--- utils/get_file_paths.py
+++ utils/get_file_paths.py
@@ -1,25 +1,20 @@
 from pathlib import Path
 
 
-def train_data():
-    train_data_path = Path('Train-Data')
-    healthy_path = train_data_path / 'NoSRF'
-    srf_path = train_data_path / 'SRF'
-    healthy_images = [file for file in healthy_path.iterdir()]
-    srf_images = [file for file in srf_path.iterdir()]
-
 
 def get_healthy_data():
     train_data_path = Path('Train-Data')
     healthy_path = train_data_path / 'NoSRF'
-    healthy_images = [file for file in healthy_path.iterdir()]
+    # healthy_images = [file for file in healthy_path.iterdir()]
+    healthy_images = list(healthy_path.glob('*.png'))
     return healthy_images
 
 
 def get_srf_data():
     train_data_path = Path('Train-Data')
     srf_path = train_data_path / 'SRF'
-    srf_images = [file for file in srf_path.iterdir()]
+    # srf_images = [file for file in srf_path.iterdir()]
+    srf_images = list(srf_path.glob('*.png'))
     return srf_images
 
 
