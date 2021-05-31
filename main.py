fd61cc4742b4260e80ec6adc3906545c0d344e8e
Boris Gusev <bgusev@gmail.com>
2021-05-31 16:15:09 +0200

    added script to create final csv file

---
+++ main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import get_img_paths
+import find_blobs
+import image_preprocessing
+
+if __name__ == '__main__':
+    output_filename = 'gusev_solis.csv'
+    col_names = ['filename', 'labels']
+    output = pd.DataFrame(columns = col_names)
+    test_imgs = get_img_paths.test_data()
+
+    img_names = list(map(lambda x: x.name, test_imgs))
+
+    labels = []
+    for img_path in tqdm(test_imgs):
+        img = plt.imread(img_path)
+        img = image_preprocessing.preprocess(img)
+
+        blobs = find_blobs.find_dark_blobs(img)
+        blobs = find_blobs.filter_blobs(img, blobs)
+
+        if len(blobs) != 0:
+            labels.append(1)
+        else:
+            labels.append(0)
+    
+    output.filename = img_names
+    output.labels = labels
+
+    output.to_csv(output_filename, index = False)
