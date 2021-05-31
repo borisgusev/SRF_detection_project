8c9067951af661eba4703ec2f7457eb39d835b92
Boris Gusev <bgusev@gmail.com>
2021-05-31 14:29:00 +0200

    sorted labels by brightness, cleanup, docstrings

--- utils/segmentation.py
+++ utils/segmentation.py
@@ -1,19 +1,25 @@
-from skimage import color, feature, filters, segmentation, morphology, measure
+from skimage import color 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import ndimage
 from sklearn import cluster
 from tqdm import tqdm
 from pathlib import Path
 
 import image_preprocessing
-import get_file_paths
-import find_blobs
-import image_edit_utils as utls
-import retina_mask
+import get_img_paths
 
-def segmentation(gray_image, nclust=3): 
+def segmentation(gray_image, nclust=3):
+    """Applies K-Means segmentation to gray image, returning segmented image and labels matrix
 
+    The matrix contains labels in range(nclust) in the order of brightness
+
+    Args:
+        gray_image (Image Array): grayscale image
+        nclust (int, optional): number of means to be used in K-means. Defaults to 3.
+
+    Returns:
+        (Img, Label_Array): Tuple of segmented image and label array
+    """
     image = color.gray2rgb(gray_image)
 
     x, y, z = image.shape
@@ -24,13 +30,27 @@ def segmentation(gray_image, nclust=3):
     kmeans_cluster.fit(image_2d)
     cluster_centers = kmeans_cluster.cluster_centers_
     cluster_labels = kmeans_cluster.labels_
-    image_segmented = cluster_centers[cluster_labels].reshape(x, y, z)
-
+    segmented_img = cluster_centers[cluster_labels].reshape(x, y, z)
     cluster_labels_matrix = cluster_labels.reshape(x, y)
 
-    return (image_segmented, cluster_labels_matrix)
+    # relabel the clusters such that 0 is darkest and nclust-1 is brightest
+    sorted_labels = _sort_labels(segmented_img, cluster_labels_matrix)
+    new_labels_matrix = np.zeros_like(cluster_labels_matrix)
+    for i in range(nclust):
+        new_labels_matrix[cluster_labels_matrix == sorted_labels[i]] = i
+    cluster_labels_matrix = new_labels_matrix
+    return segmented_img, cluster_labels_matrix
 
-def sort_labels(segmented_image, lables):
+def _sort_labels(segmented_image, lables):
+    """Returns a list of labels in the order of brightness
+
+    Args:
+        segmented_image (Image Array): Segmented Image
+        lables (Array): Array of labels for each pixel
+
+    Returns:
+        [int]: List of labels in the order of brightness
+    """
     label_brightness = []
     for lable in range(np.max(lables)+1):
         label_brightness.append(np.max(segmented_image[lables == lable]))
@@ -38,68 +58,28 @@ def sort_labels(segmented_image, lables):
     return sorted_labels
 
 if __name__ == '__main__':
-    healthy, srf = get_file_paths.get_all_train_data()
-
-    img = plt.imread(srf[0])
-    img = image_preprocessing.preprocess(img)
-    # blur = filters.gaussian(img, sigma=15)
-    # edges = feature.canny(img, sigma = 5)
-
-    mask = retina_mask.retinal_mask(img)
-
-    seg_img, cluster_labels = segmentation(img, nclust = 3)
-    # modified = seg_img
-    modified = cluster_labels != 0
-    # modified = ndimage.binary_fill_holes(modified)
-
-
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(img, cmap='gray')
-    # plt.subplot(1, 3, 2)
-    # plt.imshow(mask, cmap='gray')
-    plt.subplot(1, 2, 2)
-    plt.imshow(modified, cmap='gray')
-    plt.show()
-
-    # fig, axes = find_blobs.plot_before_after(img)
-    # plt.tight_layout()
-    # plt.show()
-
-    # fix, ax = filters.try_all_threshold(modified)
-    # plt.show()
+    # Script to run segmentation on all training images
     output_path = Path('segmentation_output')
     output_path.mkdir(exist_ok=True)
-    healthy, srf = get_file_paths.get_all_train_data()
+    healthy, srf = get_img_paths.train_data()
 
-    # img = plt.imread(srf[3])
-    # img = image_preprocessing.preprocess(img)
-    # fig, axes = plot_before_after(img)
-    # plt.tight_layout()
-    # plt.show()
+    nclust = 6
 
     healthy_output_path = output_path / 'NoSRF'
     healthy_output_path.mkdir(exist_ok=True)
     for img_path in tqdm(healthy):
         img = plt.imread(img_path)
         img = image_preprocessing.preprocess(img)
-        # mask = retina_mask.retinal_mask(img)
-
-        blurred = filters.gaussian(img, sigma = 1)
-        seg_img, cluster_labels = segmentation(blurred, nclust = 6)
-        sorted_labels = sort_labels(seg_img, cluster_labels)
-        # modified = cluster_labels == sorted_labels[1]
-        modified = seg_img
-        # modified = ndimage.binary_fill_holes(modified)
-
-
+        seg_img, cluster_labels = segmentation(img, nclust = nclust)
+        # Uncomment one below if want segmented image or particular segment
+        # modified = seg_img
+        modified = cluster_labels == 1
 
         plt.subplot(1, 2, 1)
         plt.imshow(img, cmap='gray')
-        # plt.subplot(1, 3, 2)
-        # plt.imshow(mask, cmap='gray')
         plt.subplot(1, 2, 2)
         plt.imshow(modified, cmap='gray')
+        plt.suptitle(img_path.name)
         plt.tight_layout()
         file_name = healthy_output_path / img_path.name
         plt.savefig(file_name, dpi=400, bbox_inches='tight')
@@ -110,24 +90,17 @@ if __name__ == '__main__':
     for img_path in tqdm(srf):
         img = plt.imread(img_path)
         img = image_preprocessing.preprocess(img)
-        # mask = retina_mask.retinal_mask(img)
-
-        blurred = filters.gaussian(img, sigma = 1)
-        seg_img, cluster_labels = segmentation(blurred, nclust = 6)
-        sorted_labels = sort_labels(seg_img, cluster_labels)
-        # modified = cluster_labels == sorted_labels[1]
-        modified = seg_img
-        # modified = ndimage.binary_fill_holes(modified)
-
-
+        seg_img, cluster_labels = segmentation(img, nclust = nclust)
+        # Uncomment one below if want segmented image or particular segment
+        # modified = seg_img
+        modified = cluster_labels == 1
 
         plt.subplot(1, 2, 1)
         plt.imshow(img, cmap='gray')
-        # plt.subplot(1, 3, 2)
-        # plt.imshow(mask, cmap='gray')
         plt.subplot(1, 2, 2)
         plt.imshow(modified, cmap='gray')
+        plt.suptitle(img_path.name)
         plt.tight_layout()
-        file_name = srf_output_path / img_path.name
+        file_name = healthy_output_path / img_path.name
         plt.savefig(file_name, dpi=400, bbox_inches='tight')
         plt.close()
