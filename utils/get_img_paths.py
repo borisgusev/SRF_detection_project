c328e4ae1b8fa09e0bb83fe7ec0cf654f11e741b
Boris Gusev <bgusev@gmail.com>
2021-05-31 13:34:29 +0200

    added docstrings

--- utils/get_img_paths.py
+++ utils/get_img_paths.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
 
-def train_data(path_str = 'Train-Data'):
+def train_data(path_str='Train-Data'):
+    """Returns a lists of healthy and srf image paths from training data folder
+
+    Args:
+        path_str (str, optional): path to folder containing SRF and NoSRF folders with images. Defaults to 'Train-Data'.
+
+    Returns:
+        ([Path], [Path]): tuple of two lists; each list containing paths for images
+    """
     path = Path(path_str)
     return _get_healthy_data(path), _get_srf_data(path)
+
 
 def _get_healthy_data(path):
     healthy_path = path / 'NoSRF'
@@ -16,7 +25,16 @@ def _get_srf_data(path):
     srf_images = list(srf_path.glob('*.png'))
     return srf_images
 
-def test_data(path_str = 'Test-Data'):
+
+def test_data(path_str='Test-Data'):
+    """Returns a list of test data images
+
+    Args:
+        path_str (str, optional): path towards test data folder. Defaults to 'Test-Data'.
+
+    Returns:
+        [Path]: list of image paths
+    """
     path = Path(path_str)
     img_paths = list(path.glob('*png'))
     return img_paths
